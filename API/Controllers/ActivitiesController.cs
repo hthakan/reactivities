5005f1a742d6b713fdf219b58397bf46fc4812e3
hakan <hkntspnr@gmail.com>
2021-02-24 17:36:29 +0300

    feat: initial sources

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseController
+    {
+        private readonly DataContext _dataContext;
+
+        public ActivitiesController(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+    }
+}
